2b3e9982895ca9c630b2ce7cbe8362b4083a3ab4
relar <relardev@gmail.com>
2024-01-17 21:03:10 +0100

    cleanup code a bit

--- lib/jk_elixir.ex
+++ lib/jk_elixir.ex
@@ -3,31 +3,28 @@ defmodule JkElixir do
   Documentation for `JkElixir`.
   """
 
-  defp val(acc, v) when is_map(v) do
-    keys(acc, v)
+  defp val(v) when is_map(v) do
+    keys(v)
   end
 
-  defp val(_acc, _v), do: 1
+  defp val(_v), do: 1
 
-  defp keys(_acc, record) do
+  defp keys(record) do
     record
     |> Enum.reduce(%{}, fn {key, v}, acc ->
-      Map.put(acc, key, val(acc, v))
+      Map.put(acc, key, val(v))
     end)
   end
 
-  defp to_base(v) when is_map(v), do: v
-  defp to_base(v) when is_number(v), do: 1
-  defp to_base(v) when is_list(v), do: 1
-  defp to_base(v) when is_binary(v), do: 1
-  defp to_base(v) when is_atom(v), do: 1
-  defp to_base(v) when is_boolean(v), do: 1
-  defp to_base(v) when is_nil(v), do: 1
+  defp to_base(v) when is_nil(v), do: 0
+  defp to_base(v), do: v
 
   defp merge(map1, map2) when is_map(map1) and is_map(map2) do
-    keys = Enum.concat(Map.keys(map1), Map.keys(map2))
-
-    Enum.reduce(keys, %{}, fn key, acc ->
+    [map1, map2]
+    |> Enum.map(&Map.keys/1)
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.reduce(MapSet.new(), fn x, acc -> MapSet.union(x, acc) end)
+    |> Enum.reduce(%{}, fn key, acc ->
       v1 = Map.get(map1, key)
       v2 = Map.get(map2, key)
       Map.put(acc, key, merge(v1, v2))
@@ -35,14 +32,10 @@ defmodule JkElixir do
   end
 
   defp merge(v1, v2) do
-    b1 = to_base(v1)
-    b2 = to_base(v2)
-
-    case {b1, b2} do
-      {1, 1} -> 1
-      {1, v} -> v
-      {v, 1} -> v
-      {_, _} -> merge(v1, v2)
+    case {to_base(v1), to_base(v2)} do
+      {x, y} when is_number(x) and is_number(y) -> x + y
+      {x, y} when is_number(x) -> y
+      {x, y} when is_number(y) -> x
     end
   end
 
@@ -50,7 +43,7 @@ defmodule JkElixir do
     File.stream!(path)
     |> Stream.map(&Jason.decode!/1)
     |> Enum.reduce(%{}, fn record, acc ->
-      new = keys(%{}, record)
+      new = keys(record)
       merge(acc, new)
     end)
     |> Jason.encode!()
