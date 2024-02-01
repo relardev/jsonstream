dffc6a987e3be573c3d63745359dfc1fae5b77f3
relar <relardev@gmail.com>
2024-02-01 21:58:11 +0100

    add more indepth key analysis

--- lib/keys.ex
+++ lib/keys.ex
@@ -35,7 +35,41 @@ defmodule Keys do
     keys(v)
   end
 
+  defp val([hd | _t] = v) when is_map(hd) do
+    reduced =
+      v
+      |> Enum.map(&keys/1)
+      |> Enum.reduce(%{}, fn x, acc ->
+        Map.merge(x, acc, fn _, v1, v2 -> merge(v1, v2) end)
+      end)
+
+    averaged =
+      Enum.reduce(reduced, %{}, fn {k, val}, acc ->
+        Map.put(acc, k, avg(val, length(v)))
+      end)
+
+    [averaged]
+  end
+
   defp val(_v), do: 1
+
+  defp avg(v, len) when is_map(v) do
+    Enum.reduce(v, %{}, fn {k, val}, acc ->
+      Map.put(acc, k, avg(val, len))
+    end)
+  end
+
+  defp avg([v], len) when is_map(v) do
+    [avg(v, len)]
+  end
+
+  defp avg(v, len) when is_number(v) do
+    v / len
+  end
+
+  def merge([v1], [v2]) when is_map(v1) and is_map(v2) do
+    [merge(v1, v2)]
+  end
 
   def merge(map1, map2) when is_map(map1) and is_map(map2) do
     [map1, map2]
